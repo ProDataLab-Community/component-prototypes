3be38e502cd4ef70644322d03033c7b7f9125383
Peter Alexander <makeroftools@creativedevelopment.systems>
2020-08-08 23:01:04 -0400

    cleaning and added task.py

--- src/languages/python/simple_component.py
+++ src/languages/python/simple_component.py
@@ -2,7 +2,6 @@ import zmq
 import msgpack as mp
 import task
 
-
 """
 PUB = 1
 SUB = 2
@@ -18,25 +17,12 @@ msg = message
 ctx = zmq.Context()
 ss = socket_list
 t = topic
-
 """
 
 EP_TYPES = [zmq.PUB, zmq.SUB, zmq.PUSH, zmq.PULL, zmq.REQ, zmq.REP,
             zmq.XPUB, zmq.XPUB, zmq.XSUB, zmq.STREAM, zmq.DEALER, zmq.ROUTER]
-
 ctx = zmq.Context()
 plr = zmq.Poller()
-
-
-
-
-def task(msg):
-    # COPY AND PASTE CODE HERE       ..for now
-    pass
-
-
-
-
 
 def config(config_msg):
 
@@ -64,41 +50,30 @@ def config(config_msg):
         plr.register(s, zmq.POLLOUT)
         out_sockets.append
 
-
 def kill():
     ctx.destroy()
-
 
 def encode(msg):
     return mp.packb(msg)
 
-
 def decode(pkt):
     return mp.unpackb(pkt)
 
-
-
 def run(config_endpoint):
-
-    # Initialize poll set
+                                # Initialize poll set
     plr = zmq.Poller()
-
-    # Initialize configuration socket. The first msg is alway the configuration message.
+                                # Initialize configuration socket. The first msg is alway the configuration message.
     s = ctx.socket(zmq.SUB)
     s.connect(config_endpoint)
     s.setsockopt(zmq.SUBSCRIBE, b'10001')
     plr.register(s, zmq.POLLIN)
-
-    # Process messages from sockets
-
+                                # Process messages from sockets
     while True:
         try:
             plr_ss = dict(plr.poll())
-
         except KeyboardInterrupt:
                 break
-        for ps in plr_ss:
-                                            # FIXME .. handle POLLERR, etc.       zmq.POLLERR, zmq.POLLOUT
+        for ps in plr_ss:                                       # FIXME .. handle POLLERR, etc.       zmq.POLLERR, zmq.POLLOUT
             if plr_ss[ps] != zmq.POLLIN:
                 continue                        
             msg = decode(ps.recv())
